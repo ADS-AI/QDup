de9fe983aff60bc81293ac20c28843017283adfe
Sanyam-oss <sanyam20116@iiitd.ac.in>
2022-08-20 19:40:26 +0530

    bulk_processing

--- src/ui.py
+++ src/ui.py
@@ -1,5 +1,12 @@
 import gradio as gr
-from master import run_model
+#from master import run_model
+import pandas as pd
+import json
+
+def run_model(Question, Answer=""):
+    list1 = ['first three nearest neighbour distance for primitive cubic lattice are respectively edge length of unit cell a']
+    list2 = ['first three nearest neighbour distance for body centered cubic lattice are respectively', 'first three nearest neighbour distance for body centered cubic lattice are respectively']
+    return list1, list2
 
 def list_to_string(list):
     string = ""
@@ -18,12 +25,37 @@ def master_of_masters(Question, Answer=""):
 
     return ans1, ans2
 
+def master_of_masters_file(question_file):
+    
+    print(question_file.name)
+    df = pd.read_csv(question_file.name,delimiter=',')
+    
+    df.fillna("",inplace=True)
+    
+    questions = df['question'].tolist()
+    answers = df['answer'].tolist()
+
+    print(answers)
+
+    json_out = {}
+
+    for i in range(len(questions)):
+        
+        list1, list2 = run_model(questions[i], answers[i])
+        json_out[questions[i]] = {"duplicate" : list1, "related" : list2}
+
+    print(json_out)
+
+    with open("output.json", "w") as outfile:
+        json.dump(json_out, outfile)
+
+    return "output.json"
+
 question_text = gr.Textbox(lines = 2, placeholder="Enter Question..")
 answer_text = gr.Textbox(lines = 2, placeholder="Enter Answer or Leave it empty..")
 
 output1 = gr.Textbox(label = "Question Duplicates") 
 output2 = gr.Textbox(label = "Related questions") 
-
 
 desc = ("An application to prevent academicians from creating duplicate questions by suggesting near duplicates of a question present in the database.  \n"
 
@@ -34,15 +66,43 @@ article = ("Question Duplicates :  \n"
         "Related Questions : "
         )
 
-gui = gr.Interface(
-    fn = master_of_masters,
-    inputs=[question_text, answer_text],
-    outputs=[output1, output2],
-    title = "Online Question Duplicity Finder",
-    description = desc,
-    article=article
-)
 
-gui.launch(share = True)
+with gr.Blocks() as demo:
+
+    gr.Markdown("## Online Question Duplicity Finder")
+    gr.Markdown(desc)
+
+    with gr.Tabs():
+        
+        with gr.TabItem("Add Question"):
+
+            with gr.Row():
+
+                with gr.Column() :
+            
+                    question_text = gr.Textbox(lines = 2, placeholder="Enter Question..", label="Question")
+                    answer_text = gr.Textbox(lines = 2, placeholder="Enter Answer or Leave it empty..", label="Answer")
+
+                    text_button = gr.Button("Submit")
+
+                with gr.Column() :
+                    output1 = gr.Textbox(label = "Question Duplicates") 
+                    output2 = gr.Textbox(label = "Related questions")
+
+        with gr.TabItem("Add CSV File"):
+
+            with gr.Row():
+                
+                question_file = gr.File(type = "file" , label="Question File", interactive=True)
+                output_file = gr.File(label="Duplicate and Related Questions File")
+
+            csv_button = gr.Button("Submit")
+
+        text_button.click(master_of_masters, inputs=[question_text, answer_text], outputs=[output1, output2])
+        csv_button.click(master_of_masters_file, inputs=[question_file], outputs=[output_file])
+
+    gr.Markdown(article)
+
+demo.launch()
 
 # print(list_to_string(["what is the meaning of life?", "ccghcghc42"]))
